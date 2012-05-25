ea048e22bc1b8ee9723fbbaf3b6f488fa6857870
Don Pavlik <git@donpavlik.net>
2012-05-25 15:06:37 -0400

    Fix of Time Line Entries Test

--- Model/TimeLine.cs
+++ Model/TimeLine.cs
@@ -3,12 +3,25 @@ namespace DonPavlik.Domain.Model
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
 	using System.Linq;
 	using System.Text;
 	using System.Threading.Tasks;
+	using DonPavlik.Domain.Interfaces;
 
+	/// <summary>
+	/// Time Line Class definition for working with Time Lines
+	/// </summary>
 	public class TimeLine
 	{
+		/// <summary>
+		/// Initializes a new instance of the TimeLine Class
+		/// </summary>
+		public TimeLine()
+		{
+			this.Entries = new Collection<ITimeLineEntry>();
+		}
+
 		/// <summary>
 		/// Gets the collection of entries for the time line
 		/// </summary>
